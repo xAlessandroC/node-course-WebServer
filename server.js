27be8f40b1b1e59a6247c23aafa08bc77e85811b
xAlessandroC <alexcalvio@hotmail.it>
2018-11-30 18:43:55 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");   //esprime il supporto ai partials
+app.set("view engine", "hbs");  //render con estensione hbs
+app.use(express.static(__dirname + "/public")); //dirname -> PATH del direttorio del progetto
+
+//helpers
+//sono template per funzioni che possono essere riusate
+hbs.registerHelper("getCurrentYear",()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt",(text)=>{
+  return text.toUpperCase();
+});
+
+//express middleware
+app.use((req,res, next)=>{
+  var now=new Date().toString();
+  var log=`${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log',log+"\n",(err)=>{
+    if(err){
+      console.log("[ERROR]: Unable to log request");
+    }
+  })
+  next();
+});
+
+app.use((req,res, next)=>{
+    res.render("maintenanceTemplate.hbs",{
+      titlePage:"Under maintenance",
+      maintenanceMessage:"Site will be up soon!",
+    })
+});
+
+app.get("/",(req,res)=>{
+  res.render("homeTemplate.hbs",{
+    titlePage:"Main Page",
+    welcomeMessage:"Hey, Welcome to my website!",
+    //currentYear: new Date().getFullYear()
+  })
+});
+
+app.get("/about",(req,res)=>{
+  res.render("aboutTemplate.hbs",{
+    titlePage:"About Page",
+    //currentYear: new Date().getFullYear()
+  })
+});
+
+app.get("/bad",(req,res)=>{
+  res.send({
+    errorMessage:"Something gone wrong!"
+  });
+});
+
+app.listen("3000",()=>{
+  console.log("Server listening on port 3000!");
+});
